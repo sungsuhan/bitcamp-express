9e4955447eac6eb28581256fcf1b4d7798ac2ace
sungsuhan <swhanssu@gmail.com>
2022-04-15 12:46:17 +0900

    2022-04-15 12:46 join

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,6 +2,8 @@ const db = require('../models/index')
 const UserSchema = db.user
 
 exports.join = (req,res) => {
+    console.log(' ### 진행 4: 노드서버에 진입함' + JSON.stringify(req.body))
+    
     new UserSchema(req.body).save(()=>{
         res.status(200).json({'result':'ok'}) 
     })
@@ -9,12 +11,12 @@ exports.join = (req,res) => {
 }
 
 exports.login = (req,res) => {
-    const {username, password} = req.body
+    const {userid, password} = req.body
     console.log(`넘어온 JSON 값 : ${JSON.stringify(req.body)}`)
-    console.log(`ID 값 : ${username}`)
+    console.log(`ID 값 : ${userid}`)
     console.log(`PW 값 : ${password}`)
     const user = new UserSchema({
-        username, password
+        userid, password
     })
     user.save(() => {
         res.status(200).json({'result':'ok'})
@@ -32,7 +34,7 @@ exports.userlist = (req,res) => {
 
 exports.profile = (req, res) => {
     console.log(`### user profile access ###`)
-    UserSchema.find({username: req.params.id})
+    UserSchema.find({userid: req.params.id})
     .exec((err, user) => {
         if (err) return res.status(400).send(err)
         res.status(200).json({ success: true, user })
