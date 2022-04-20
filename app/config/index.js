f920157e65820c265851e518bc8753ed9f331459
sungsuhan <swhanssu@gmail.com>
2022-04-20 12:29:03 +0900

    2022-04-20 12:28 es6

---
+++ app/config/index.js
@@ -0,0 +1,7 @@
+import dbConfig from './dbConfig';
+import nextConfig from './nextConfig';
+
+export default {
+  dbConfig,
+  nextConfig,
+};
--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 import dotenv from 'dotenv'
 import express from 'express';
 import cors from 'cors'
-import db from './app/models/index.js'
-import tokenRouter from './app/routes/token.js';
-import todoController from './app/controllers/todoController.js'
-import userController from './app/controllers/userController.js'
+//import db from './app/models/index.js'
+//import tokenRouter from './app/routes/token.js';
+//import todoController from './app/controllers/todoController.js'
+//import userController from './app/controllers/userController.js'
 
 async function startServer() {
     dotenv.config()
     const app = express();
     const mongoUri = process.env.MONGO_URI
-    const port = process
-        .env
-        .PORT
-        app
-        .use('/token', tokenRouter);
+    const port = process.env.PORT
+    //app.use('/token', tokenRouter);
     app.use(express.static('public'));
     app.use(express.urlencoded({extended: true}));
     app.use(express.json());
     app.use(cors());
-    app.use('/todo', todoController)
-    app.use('/user', userController)
+    //app.use('/todo', todoController)
+    //app.use('/user', userController)
 
     const corsOptions = {
         origin: 'http://localhost:3000',
         optionsSuccessStatus: 200
     }
 
-    db
+    /*db
         .mongoose
         .connect(mongoUri, {
             useNewUrlParser: true,
@@ -39,7 +36,7 @@ async function startServer() {
         .catch(err => {
             console.log(' 몽고DB와 연결 실패', err)
             process.exit();
-        });
+        });*/
     app.listen(port, () => {
         console.log('***************** ***************** *****************')
         console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
